4aa5515a55d9d701cc4359acbbe81ee0d7663040
Gulshan <taksandegulshan@gmail.com>
2021-04-23 20:31:42 +0530

    commit

--- daily_diary/settings.py
+++ daily_diary/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -126,3 +128,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ],
 }
+
+django_heroku.settings(locals())
